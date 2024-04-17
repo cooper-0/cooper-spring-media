68e76cfc1542ac6f0e100e35c2e429ac5930f9c3
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-18 00:24:46 +0900

    init

---
+++ src/test/java/com/whisper/coopermedia/CooperMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whisper.coopermedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
