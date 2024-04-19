f1f74856b5694b87c79f4e16ca53220c140052f6
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-19 17:32:04 +0900

    BaseEntity 추가
    데이터 삽입, 수정 시간 추가

--- src/main/java/com/whisper/coopermedia/entity/MediaChannel.java
+++ src/main/java/com/whisper/coopermedia/entity/MediaChannel.java
@@ -1,18 +1,15 @@
 package com.whisper.coopermedia.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
+@Table(name = "media_channels")
 @Entity
-@Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MediaChannel {
+public class MediaChannel extends BaseEntity {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY) // db가 id를 자동 생성하게
     private Long id;
