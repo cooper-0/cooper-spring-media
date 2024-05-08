df83c9e0d763f5e0af6eb7472fcff22565fe0373
marso34 <96871583+marso34@users.noreply.github.com>
2024-05-09 00:31:02 +0900

    주석 수정

--- src/main/java/com/whisper/coopermedia/controller/SignalingController.java
+++ src/main/java/com/whisper/coopermedia/controller/SignalingController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class SignalingController {
-    //offer 정보를 주고 받기 위한 websocket
-    //camKey : 각 요청하는 캠의 key , roomId : 룸 아이디
+    /**
+     * offer 정보를 주고 받기 위한 websocket
+     * @param roomId 룸 아이디
+     * @param camKey 각 요청하는 캠의 key
+     */
     @MessageMapping("/peer/offer/{camKey}/{roomId}")
     @SendTo("/topic/peer/offer/{camKey}/{roomId}")
     public String PeerHandleOffer(@Payload String offer, @DestinationVariable(value = "roomId") String roomId,
@@ -20,8 +23,11 @@ public class SignalingController {
         return offer;
     }
 
-    //iceCandidate 정보를 주고 받기 위한 webSocket
-    //camKey : 각 요청하는 캠의 key , roomId : 룸 아이디
+    /**
+     * iceCandidate 정보를 주고 받기 위한 webSocket
+     * @param roomId 룸 아이디
+     * @param camKey 각 요청하는 캠의 key
+     */
     @MessageMapping("/peer/iceCandidate/{camKey}/{roomId}")
     @SendTo("/topic/peer/iceCandidate/{camKey}/{roomId}")
     public String PeerHandleIceCandidate(@Payload String candidate, @DestinationVariable(value = "roomId") String roomId,
@@ -30,7 +36,9 @@ public class SignalingController {
         return candidate;
     }
 
-
+    /**
+     *
+     */
     @MessageMapping("/peer/answer/{camKey}/{roomId}")
     @SendTo("/topic/peer/answer/{camKey}/{roomId}")
     public String PeerHandleAnswer(@Payload String answer, @DestinationVariable(value = "roomId") String roomId,
@@ -39,7 +47,9 @@ public class SignalingController {
         return answer;
     }
 
-    //camKey 를 받기위해 신호를 보내는 webSocket
+    /**
+     * camKey 를 받기위해 신호를 보내는 webSocket
+     */
     @MessageMapping("/call/key")
     @SendTo("/topic/call/key")
     public String callKey(@Payload String message) {
@@ -47,7 +57,9 @@ public class SignalingController {
         return message;
     }
 
-    //자신의 camKey 를 모든 연결된 세션에 보내는 webSocket
+    /**
+     * 자신의 camKey 를 모든 연결된 세션에 보내는 webSocket
+     */
     @MessageMapping("/send/key")
     @SendTo("/topic/send/key")
     public String sendKey(@Payload String message) {
